1ae3960a905f321244d3caa6af060d9a08a8db16
Phillip Laclede <prlaclede@gmail.com>
2015-11-20 01:19:29 +0000

    bundled css and javascript

--- public_html/etg/__init__.py
+++ public_html/etg/__init__.py
@@ -2,6 +2,7 @@ import os, logging, md5
 from flask import (Flask, session, render_template, request, redirect,
 url_for, jsonify, json)
 from flask.ext.mail import Mail
+from flask.ext.assets import Environment, Bundle
 from smtplib import SMTPException
 from UserAPI import user_api
 from EssayAPI import essay_api
@@ -25,12 +26,30 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+
 app.register_blueprint(user_api)
 app.register_blueprint(essay_api)
 app.register_blueprint(marker_api)
 app.register_blueprint(driveAccess_api)
+assets = Environment(app)
 
 emailConfigs = Email.configProperties
+
+customJS = Bundle('js/custom/etg.js', 'js/custom/maps.js')
+pluginJS = Bundle('js/plugins/jquery-1.11.3.js', 'js/plugins/bootstrap/bootstrap.js')
+
+all_JS = Bundle(
+    Bundle('js/plugins/jquery-1.11.3.js', 'js/plugins/bootstrap/bootstrap.js', 
+    'https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDeovcMJI1fqgbiZeyKwNDiBI3N8ghcmEc&dummy=.js'),
+    Bundle('js/custom/etg.js', 'js/custom/maps.js', 'js/custom/accLogic.js'))
+    
+all_CSS = Bundle(
+    Bundle('css/plugins/bootstrap/bootstrap.css', 'css/plugins/animate.css'),
+    Bundle('css/custom/icons.css', 'css/custom/style.css'))
+
+assets.register('all_CSS', all_CSS)    
+assets.register('all_JS', all_JS)
+#assets.register('js_plugin', pluginJS)
 
 for key, value in emailConfigs.iteritems():
     app.config.update(key = value)
--- public_html/etg/static/js/custom/etg.js
+++ public_html/etg/static/js/custom/etg.js
@@ -1,14 +1,8 @@
-//$(document).ready(function() {
 $(function (etgLogic, $, undefined) {
     //TODO: incapsulate into INIT function for extendability purposes
     $('[data-toggle="popover"]').popover();
     $('#splash_modal').modal('show');
     $("#svg-icons").load("/static/img/icons.svg");
-    $(document).keypress(function(e){
-        if (e.which == 13){
-            $(".splashLoginButton").click();
-        }
-    });
     
     $.getJSON('/loadRecentEssays', function(response) {
        var essayList = response['essayList'];
@@ -29,144 +23,11 @@ $(function (etgLogic, $, undefined) {
        });
     });
 
-    $.getJSON('/checkUser', function(response) {
-        var user = response['user'];
-        if (user != null) {
-            loadUser(user);
-        }
-    });
-    
     $('body')
-    
-        .on('click', '.loginButton', function() {
-            var thisForm = $(this).closest('#loginForm');
-            if (!isValidEmailAddress(thisForm.find('.emailField').val())) {
-                thisForm.closest('.modal-body').before(generateAlert('warning', 'Invalid Email!'));
-            } else {
-                $.ajax({
-                    url: '/login',
-                    data: thisForm.serialize(),
-                    type: 'POST',
-                    success: function (response) {
-                        if (response != undefined) {
-                            var user = response['user'][0];
-                            if (user != undefined) {
-                                thisForm.closest('.modal-body').before(generateAlert('success', 'Sucessful login!'));
-                                $('#splash_modal').modal('hide');
-                                $('#accountActionPopup').modal('hide');
-                                loadUser(user);
-                            } else {
-                                thisForm.closest('.modal-body').before(generateAlert('warning', 'Email and/or Password information is incorrect!'));
-                            }
-                        }
-                    },
-                    error: function (error) {
-                        console.log("error" + JSON.stringify(error));
-                    }
-                });
-            }
-        })
-    
-        .on('click', '.registerButton', function() {
-            var thisForm = $(this).closest('#registerForm');
-            if (!isValidEmailAddress(thisForm.find('.emailField').val())) {
-                thisForm.closest('.modal-body').before(generateAlert('warning', 'Invalid Email!'));
-            } else {
-                $.ajax({
-                    url: '/register',
-                    data: thisForm.serialize(),
-                    type: 'POST',
-                    success: function (response) {
-                        if (response['error']) {
-                            var emailParams = response['emailParams'];
-                            thisForm.closest('.modal-body').before(generateAlert('warning', 'That email has already been registered!'));
-                        } else {
-                            var emailParams = response['emailParams'];
-                            thisForm.closest('.modal-body').before(generateAlert('success', 'You will recieve an email when your account has been approved.'));
-                        }
-                    },
-                    error: function (error) {
-                        console.log("error" + JSON.stringify(error));
-                    }
-                });
-            }
-        })
-    
-        .on('hide.bs.modal', '#splash_modal', function () {
-            $('.alert').remove();
-            $('#accountActionSpan').show();
-            $('#accountActionButton').html('Login');
-            $('#accountRegisterButton').show();
-            $('#welcomeMessage').html('Welcome Guest');
-        })
     
         .on('click', '.mapMode', function() {
             $('.mapMode').closest('li').removeClass('selectedMode');
             $(this).closest('li').toggleClass('selectedMode'); 
-        })
-        
-        .on('click', '#accountActionButton', function() {
-            console.log('checking this');
-            var buttonText = $(this).html();
-            if (buttonText == 'Login') {
-                $.ajax({
-                    url: '/getLoginForm',
-                    type: "POST",
-                    contentType: 'application/json',
-                    dataType: "html",
-                    success: function(response) {
-                        //clear old form, append newly ajax'd form
-                        $('#accountActionPopup').find('.modal-body').html('').append(response);
-                        $('#accountActionPopup').modal('show').find('.modal-title').html('Login');  
-                    },
-                    error: function (error) {
-                        return("error" + JSON.stringify(error));
-                    }
-                });
-            } else if (buttonText == 'Logout') {
-                $.getJSON('/logout').done(function (response) {
-                    window.location.reload();
-                });
-            } 
-        })
-        
-        .on('click', '#accountRegisterButton', function() {
-            $.ajax({
-                url: '/getRegisterForm',
-                type: "POST",
-                contentType: 'application/json',
-                dataType: "html",
-                success: function(response) {
-                    //clear old form, append newly ajax'd form
-                    $('#accountActionPopup').find('.modal-body').html('').append(response);
-                    $('#accountActionPopup').modal('show').find('.modal-title').html('Register');
-                },
-                error: function (error) {
-                    return("error" + JSON.stringify(error));
-                }
-            });
-        })
-        
-        .on('click', '.setPasswordButton', function() {
-            var thisForm = $(this).closest('#setPasswordForm');
-            var pass1Val = thisForm.find('#password').val();
-            var pass2Val = thisForm.find('#passwordVerify').val();
-            if (pass1Val == pass2Val) {
-                $.ajax({
-                    url: '/setPassword',
-                    type: 'POST',
-                    data: thisForm.serialize(),
-                    dataType: "html",
-                    success: function(response) {
-                        window.location = response; 
-                    },
-                    error: function (error) {
-                        return("error" + JSON.stringify(error));
-                    }
-                });
-            } else {
-                thisForm.closest('.modal-body').before(generateAlert("warning", "The passwords don't match!"));
-            }
         })
     
         .on('click', '#pendingUsers', function() {
@@ -346,45 +207,6 @@ $(function (etgLogic, $, undefined) {
             }).setMap(popoverMap);
         });
     
-    function loadUser (user) {
-        $('#accountActionSpan').show();
-        $('#accountRegisterButton').hide();
-        $('#accountActionButton').html('Logout');
-        $('#welcomeMessage').html("Welcome " + user['first_name'] + " " + user['last_name'])
-        $('#accountActionSpan').find('div.im').remove();
-        $.ajax({
-            url: '/getUserTools',
-            type: "POST",
-            contentType: 'application/json',
-            dataType: "html",
-            success: function(response) {
-                //add map edit tools
-                $('.featuresNav').append(response);
-            },
-            error: function (error) {
-                return("error" + JSON.stringify(error));
-            }
-        });
-        if (user['account_type_id_fk'] == 1) {
-            $.ajax({
-                url: '/getAdminTools',
-                type: "POST",
-                contentType: 'application/json',
-                dataType: "html",
-                success: function(response) {
-                    //add admin dropdown tools
-                    $('.featuresNav').append(response);
-                },
-                error: function (error) {
-                    return("error" + JSON.stringify(error));
-                }
-            });
-            $('#welcomeMessage').after(etgLogic.generateSVG('adminAccount', 'accIcon'));
-        } else {
-            $('#welcomeMessage').after(etgLogic.generateSVG('basicAccount', 'accIcon'));;
-        }
-    }
-    
     etgLogic.generateSVG = function(icon, addClass) {
       var image = "<div class='im " + addClass + "'> \
                     <svg viewBox='0 0 34 34'> \
@@ -396,7 +218,7 @@ $(function (etgLogic, $, undefined) {
       return image;
     }
     
-    function generateAlert(type, message) {
+    etgLogic.generateAlert = function (type, message) {
     $('.alert').remove();
     var alert = '<div class="alert alert-' + type + '"> \
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> \
@@ -444,11 +266,6 @@ $(function (etgLogic, $, undefined) {
                         ' + ((type === 'essays') ? '<th>View Location</th>' : '') + '\
                      </tr>'
         return header;
-    }
-    
-    function isValidEmailAddress(emailAddress) {
-        var pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-        return pattern.test(emailAddress);
     }
     
     function swapUploadToPopup(element, content) {
